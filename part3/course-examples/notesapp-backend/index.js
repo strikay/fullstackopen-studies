273590460d7242f82100801cec30fca58fd2ba02
strikay <strikay8@gmail.com>
2023-08-02 15:06:27 +0200

    Port info now derived from enviroment variables or the default port 3001

--- part3/course-examples/notesapp-backend/index.js
+++ part3/course-examples/notesapp-backend/index.js
@@ -68,7 +68,7 @@ let notes = [
     return response.json(note)
   })
   
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
