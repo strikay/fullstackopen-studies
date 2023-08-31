89c007a7ac5a82ef93704578544430059c2def61
strikay <strikay8@gmail.com>
2023-08-31 16:45:44 +0200

    Created the app.js module for handling all the application functionality

---
+++ part4/bloglist-backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const blogRouter = require('./controllers/blogs')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const mongoUrl = 'mongodb://127.0.0.1:27017/bloglist'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
--- part4/bloglist-backend/index.js
+++ part4/bloglist-backend/index.js
@@ -1,16 +1,4 @@
-const express = require('express')
-const blogRouter = require('./controllers/blogs')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const mongoUrl = 'mongodb://127.0.0.1:27017/bloglist'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogRouter)
+const app = require('./app')
 
 const PORT = 3003
 app.listen(PORT, () => {
