c42fe23dc79e438264071699ca538531c608c4c6
strikay <strikay8@gmail.com>
2023-09-01 22:04:48 +0200

    Fixed the divide by zero error

--- part3/course-examples/notesapp-backend/utils/for_testing.js
+++ part3/course-examples/notesapp-backend/utils/for_testing.js
@@ -9,7 +9,9 @@ const reverse = (string) => {
     const reducer = (sum, item) => {
       return sum + item
     }
-    return array.reduce(reducer, 0) / array.length
+    return array.length === 0
+        ? 0
+        : array.reduce(reducer, 0) / array.length
   }
   
   module.exports = {
