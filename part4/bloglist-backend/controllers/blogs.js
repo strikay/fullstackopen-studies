42df2c51b0d12c2093b3543ead8541d6c5f76a2a
strikay <strikay8@gmail.com>
2023-09-15 12:09:24 +0200

    Added the delete and put routes to the router middleware

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -12,4 +12,16 @@ blogRouter.post('/', async (request, response) => {
   response.status(201).json(result)
 })
 
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const {title, url, author} = request.body
+
+  const updated = await Blog.findByIdAndUpdate(request.params.id, {title, url, author}, { new: true })
+  response.json(updated)
+})
+
 module.exports = blogRouter
