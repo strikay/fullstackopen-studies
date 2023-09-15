05e70c0ad7f285c1c0a8946b2a4971e9a50c061a
strikay <strikay8@gmail.com>
2023-09-15 13:11:21 +0200

    Modified the noteSchema to include validation

--- part3/course-examples/notesapp-backend/models/note.js
+++ part3/course-examples/notesapp-backend/models/note.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
   important: Boolean,
 })
 
