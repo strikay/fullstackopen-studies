5f2eed6b60b5d6d44974fae29dbb9697a379f171
strikay <strikay8@gmail.com>
2023-09-24 13:27:32 +0200

    Modified the app.js file to include the users and the login routers

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('express-async-errors')
 const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const app = express()
 const cors = require('cors')
@@ -12,7 +14,12 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use(middleware.tokenExtractor)
+app.use(middleware.userExtractor)
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+
 module.exports = app
