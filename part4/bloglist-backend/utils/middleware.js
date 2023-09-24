ba9823b87c58700e10ebed89b5a57a76b4228971
strikay <strikay8@gmail.com>
2023-09-24 13:21:44 +0200

    Modified the middleware.js file to include the userExtractor and the token extractor middleware

--- part4/bloglist-backend/utils/middleware.js
+++ part4/bloglist-backend/utils/middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
@@ -8,12 +11,41 @@ const errorHandler = (error, request, response, next) => {
       return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
       return response.status(400).json({ error: error.message })
+    } else if (error.name === 'JsonWebTokenError') {
+      return response.status(400).json({ error: error.message })
     }
   
     next(error)
   }
+
+  const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+  
+    if(authorization&&authorization.startsWith('Bearer ')){
+      request.token = authorization.replace('Bearer ', '')
+    }else{
+      request.token = null
+    }
+  
+    next()
+  }
+  const userExtractor = async (request, response, next) => {
+
+
+    if(request.token){
+      const decodedToken = jwt.verify(request.token, process.env.SECRET)
+      const user = await User.findById(decodedToken.id)
+      request.user = user
+    }else{
+      request.token = null
+    }
+  
+    next()
+  }
   
   module.exports = {
     errorHandler,
-    unknownEndpoint
+    unknownEndpoint,
+    tokenExtractor,
+    userExtractor
   }
