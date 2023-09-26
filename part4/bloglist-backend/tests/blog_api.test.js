afafcacf5e09d2f497748d769a360a5bb5a3228b
strikay <strikay8@gmail.com>
2023-09-26 14:47:43 +0200

    Modified the blog api testing module to utilise authentication tokens for tests that require them. Modified the tests to utilise the test_helper module

--- part4/bloglist-backend/tests/blog_api.test.js
+++ part4/bloglist-backend/tests/blog_api.test.js
@@ -4,26 +4,32 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-const initialBlogs = [
-  {
-    title: 'He acted as Mr Bean',
-    author: 'Rowan Atkinson',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 17,
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  },
-]
+const {
+  initialBlogs, 
+  exampleUsers, 
+  authenticatedUsers, 
+  addUsers, 
+  loginUsers, 
+  addBlogs,
+  blogsInDb
+} = require('./test_helper')
+
+let bloggingUser 
+let regularUser
 
 beforeEach(async () => {
 
-await Blog.deleteMany({})
-await Blog.insertMany(initialBlogs)
+  await User.deleteMany({})
+
+  await addUsers(api)
+  await loginUsers(api)
+  bloggingUser = authenticatedUsers[0]
+  regularUser = authenticatedUsers[1]
+
+  await Blog.deleteMany({})
+  await addBlogs(bloggingUser)
 
 })
 
@@ -46,38 +52,41 @@ test('uniq identifier is named id', async () => {
 })
 
 test('making a post request successfully creates a new blog', async () => {
+  
   const dummyBlogPost =   {
     title: 'This is a test blog post',
     id: '6500a804928ff98e8e9dfc14',
     author: 'My New Audio Friend',
+    user: bloggingUser.id,
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 17,
   }
   
   await api
     .post('/api/blogs')
+    .set('Authorization', 'Bearer '+bloggingUser.token)
     .send(dummyBlogPost)
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
-  const blogs = await api.get('/api/blogs')
-  const blogs_db = await Blog.find({})
-  //This method allows the use of fomatted id in the resulting blog object
-  const blogs_db_json = blogs_db.map(blog => blog.toJSON())
-  expect(blogs_db_json).toHaveLength(initialBlogs.length+1)
-  expect(blogs_db_json).toContainEqual(dummyBlogPost)
+  let blogs = (await blogsInDb()).map(blog => blog.title)
+
+  expect(blogs).toHaveLength(initialBlogs.length+1)
+  expect(blogs).toContainEqual(dummyBlogPost.title)
 })
 
 test('missing likes property causes the value 0 to be saved in the database', async () => {
   const dummyBlogPost =  {
     title: 'This is a test blog post',
     id: '6500a804928ff98e8e9dfc14',
+    user: bloggingUser.id,
     author: 'My New Audio Friend',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
   }
 
   await api
     .post('/api/blogs')
+    .set('Authorization', 'Bearer '+bloggingUser.token)
     .send(dummyBlogPost)
     .expect(201)
 
@@ -89,31 +98,33 @@ test('missing title results in error 400 and failure to save to database ', asyn
   const dummyBlogPost =  {
     id: '6500a804928ff98e8e9dfc14',
     author: 'My New Audio Friend',
+    user: bloggingUser.id,
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
   }
 
   await api
     .post('/api/blogs')
+    .set('Authorization', 'Bearer '+bloggingUser.token)
     .send(dummyBlogPost)
     .expect(400)
 })
 
 test('a blog can be deleted', async () => {
-  const blogsAtStart = await Blog.find({})
+  const blogsAtStart = await blogsInDb()
   const blogToDelete = blogsAtStart[0]
 
   await api
     .delete(`/api/blogs/${blogToDelete.id}`)
+    .set('Authorization', 'Bearer '+bloggingUser.token)
     .expect(204)
 
-    const blogsAtEnd = await Blog.find({})
+    const blogsAtEnd = await blogsInDb()
     expect(blogsAtEnd).toHaveLength(initialBlogs.length-1)
 })
 
 test('the likes property of a blog can be updated', async () => {
 
-  let blogsAtStart = await Blog.find({})
-  blogsAtStart = blogsAtStart.map(blog => blog.toJSON())
+  let blogsAtStart = await blogsInDb()
   const blogToUpdate = blogsAtStart[0]
   const updatedBlog = {...blogToUpdate, likes:blogToUpdate.likes+1}
 
@@ -122,12 +133,35 @@ test('the likes property of a blog can be updated', async () => {
     .send(updatedBlog)
     .expect(200)
 
-    let blogsAtEnd = await Blog.find({})
-    blogsAtEnd = blogsAtEnd.map(blog => blog.toJSON())
+    let blogsAtEnd = await blogsInDb()
     expect(blogsAtEnd).not.toContainEqual(blogToUpdate)
     expect(blogsAtEnd).toContainEqual(updatedBlog)
     expect(blogsAtEnd[0].likes).toEqual(blogToUpdate.likes+1)
 })
+
+test('attempting to post a blog without providing an authentication token fails with a 401', async () => {
+  
+  const dummyBlogPost =   {
+    title: 'This is a test blog post',
+    id: '6500a804928ff98e8e9dfc14',
+    author: 'My New Audio Friend',
+    user: bloggingUser.id,
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 17,
+  }
+  
+  await api
+    .post('/api/blogs')
+    .send(dummyBlogPost)
+    .expect(401)
+    .expect('Content-Type', /application\/json/)
+
+  let blogs = (await blogsInDb()).map(blog => blog.title)
+
+  expect(blogs).toHaveLength(initialBlogs.length)
+  expect(blogs).not.toContainEqual(dummyBlogPost.title)
+})
+
 
 afterAll(async () => {
   await mongoose.connection.close()
