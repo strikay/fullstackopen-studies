778614e753df47a37a601b500a4094195c7282ce
strikay <strikay8@gmail.com>
2023-09-26 14:42:53 +0200

    Modified the error handler to send out the correct error message for missing token

--- part4/bloglist-backend/utils/middleware.js
+++ part4/bloglist-backend/utils/middleware.js
@@ -12,7 +12,7 @@ const errorHandler = (error, request, response, next) => {
     } else if (error.name === 'ValidationError') {
       return response.status(400).json({ error: error.message })
     } else if (error.name === 'JsonWebTokenError') {
-      return response.status(400).json({ error: error.message })
+      return response.status(401).json({ error: error.message })
     }
   
     next(error)
@@ -24,7 +24,7 @@ const errorHandler = (error, request, response, next) => {
     if(authorization&&authorization.startsWith('Bearer ')){
       request.token = authorization.replace('Bearer ', '')
     }else{
-      request.token = null
+      request.token = ""
     }
   
     next()
@@ -37,7 +37,7 @@ const errorHandler = (error, request, response, next) => {
       const user = await User.findById(decodedToken.id)
       request.user = user
     }else{
-      request.token = null
+      request.user = ""
     }
   
     next()
