8c9af0842614fc0c069db39ae6ff236e1171f52c
strikay <strikay8@gmail.com>
2023-10-26 21:20:41 +0200

    Modified the blogs service module. It now includes setToken fx, create fx, update fx and remove fx

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,43 @@
 import axios from 'axios'
-const baseUrl = '/api/blogs'
+const baseUrl = 'http://localhost:3003/api/blogs'
+
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const create = async (blogObject) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.post(baseUrl, blogObject, config)
+  ///const response = await request
+  return response.data
+}
+
+const update = async (id, blogObject) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  const response = await axios.put(`${baseUrl}/${id}`, blogObject, config)
+  
+  return response.data
+}
+
+const remove = async (id) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  
+  const response = await axios.delete(`${baseUrl}/${id}`, config)
+  return response
+}
+
+export default { getAll, create, update, remove, setToken }
