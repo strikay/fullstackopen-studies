94f51069aa99f0f3910874a0bce7376ba44c6eb0
strikay <strikay8@gmail.com>
2023-10-26 22:00:29 +0200

    Modified the notesapp-backend

--- part3/course-examples/notesapp-backend/utils/middleware.js
+++ part3/course-examples/notesapp-backend/utils/middleware.js
@@ -5,6 +5,7 @@ const requestLogger = (request, response, next) => {
   logger.info('Path:  ', request.path)
   logger.info('Body:  ', request.body)
   logger.info('---')
+
   next()
 }
 
@@ -19,10 +20,18 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: error.message
+    })
   }
+
 
   next(error)
 }
+
 
 module.exports = {
   requestLogger,
