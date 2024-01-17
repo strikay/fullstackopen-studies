b164f799fcf655e93a20e8b5c2720bbe631f5b14
strikay <strikay8@gmail.com>
2024-01-17 11:01:11 +0200

    Finishd the client-graphql-phonebook example

---
+++ part8/course-examples/client-graphql-phonebook/src/App.js
@@ -0,0 +1,104 @@
+import { gql, useApolloClient, useQuery, useSubscription } from '@apollo/client'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import { ALL_PERSONS, PERSON_ADDED } from './queries'
+import { useEffect, useState } from 'react'
+import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
+
+const Notify = ({errorMessage}) => {
+  if (!errorMessage ) {
+    return null
+  }
+  return (
+    <div style={{color: 'red'}}>
+    {errorMessage}
+    </div>
+  )
+}
+
+export const updateCache = (cache, query, addedPerson) => {
+  // helper that is used to eliminate saving same person twice
+  const uniqByName = (a) => {
+    let seen = new Set()
+    return a.filter((item) => {
+      let k = item.name
+      return seen.has(k) ? false : seen.add(k)
+    })
+  }
+
+  cache.updateQuery(query, ({ allPersons }) => {
+    return {
+      allPersons: uniqByName(allPersons.concat(addedPerson)),
+    }
+  })
+}
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+
+  const client = useApolloClient()
+
+  useSubscription(PERSON_ADDED, {
+    onData: ({ data }) => {
+      const addedPerson = data.data.personAdded
+      notify(`${addedPerson.name} added`)
+
+      updateCache(client.cache, { query: ALL_PERSONS }, addedPerson)
+    }
+  })
+  
+  useEffect(()=>{
+    const token = localStorage.getItem('phonenumbers-user-token')
+    setToken(token)
+  },[])
+
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  console.log(result)
+  const persons = result.data.allPersons
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
+      <Persons persons={persons}/>
+      <PersonForm setError={notify}/>
+      <PhoneForm setError={notify}/>
+    </div>
+  )
+  
+}
+
+export default App
