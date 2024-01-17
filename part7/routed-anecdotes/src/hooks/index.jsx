48def4090ca5c2e1bd5cc3e0ea26d929b4bcd5d2
strikay <strikay8@gmail.com>
2024-01-17 10:44:28 +0200

    Completed the routed-anecdotes exercise

---
+++ part7/routed-anecdotes/src/hooks/index.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const useField = (name, type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  return [
+    {
+        type,
+        name,
+        value,
+        onChange
+    },
+   reset
+  ]
+}
+
