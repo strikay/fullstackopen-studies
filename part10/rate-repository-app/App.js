1cf1b79a0e5a37c89fafd6bb797bf65f17e123bf
strikay <strikay8@gmail.com>
2024-01-17 12:38:04 +0200

    Created first version of the rate-rpository-app

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I made my first React Native App!- Strive </Text>
+      <Text>I made my first React Nativee App!- Strive </Text>
       <StatusBar style="auto" />
     </View>
   );
