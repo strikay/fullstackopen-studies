b6cb6487fc455ff111fb3891c7ee6a33acf906c6
strikay <strikay8@gmail.com>
2024-01-17 09:02:49 +0200

    Added the bootstrap distribution, Completed the Webpack Example

---
+++ part7/course-examples/webpack-part7/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+const config = () => {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js'
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'build'),
+        compress: true,
+        port: 3000,
+      },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+      devtool: 'source-map',
+  }
+}
+
+module.exports = config
